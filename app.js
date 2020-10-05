09d2995a0e7c31be0422887111bf30cce16de0b0
vutanguofa <68674610+vutanguofa@users.noreply.github.com>
2020-10-04 19:00:29 -0700

    including class paths and contructing

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const inquirer = require('inquirer');
 const generatePage = require('./src/page-template');
 const { writeFile, copyFile } = require('./utils/generate-site');
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
 
 const promptUser = () => {
   console.log(`
